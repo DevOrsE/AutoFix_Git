2309d963a59b73c973172ae4783b816f57848ee6
DevOrsE <zhenechka.antipov.98@mail.ru>
2025-04-10 11:47:40 +0300

    Обновлены записи в таблицах 'ВидРабот' и 'НомерЗапчасти': удалены упоминания моделей авто, упрощены названия работ

--- models.py
+++ models.py
@@ -2,48 +2,51 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Автомобили(db.Model):
-    __tablename__ = 'Автомобили'
-    Код_автомобиля = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Марка_автомобиля = db.Column(db.String(40), nullable=False)
-    Код_владельца = db.Column(db.Integer, db.ForeignKey('Владельцы.Код_владельца'), nullable=False)
-    Регистрационный_знак = db.Column(db.String(40), nullable=False)
-    Год_выпуска = db.Column(db.Integer, nullable=False)
-    Кузов = db.Column(db.String(40), nullable=False)
-    Примечание = db.Column(db.String(40), nullable=False)
-
 class Владельцы(db.Model):
     __tablename__ = 'Владельцы'
     Код_владельца = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Фамилия = db.Column(db.String(50))
-    Имя = db.Column(db.String(50))
-    Отчество = db.Column(db.String(50))
-    Терефон = db.Column(db.String(20))
-    Примечание = db.Column(db.String(100))
-    Логин = db.Column(db.String(50), unique=True)
-    Пароль = db.Column(db.String(256))
+    ФИО = db.Column(db.String(100), nullable=False)
+    Телефон = db.Column(db.String(20), nullable=False)
+    Логин = db.Column(db.String(50), unique=True, nullable=False)
+    Пароль = db.Column(db.String(256), nullable=False)
+    Роль = db.Column(db.String(20), nullable=False, default="user")  # user, manager, admin
 
-class НомерЗапчасти(db.Model):
-    __tablename__ = 'Номер_запчасти'
-    Номер_запчасти = db.Column(db.Integer, primary_key=True)
-    Наименование = db.Column(db.String(40), nullable=False)
-    Стоймость = db.Column(db.Float, nullable=False)
-    Гарантия = db.Column(db.Integer, nullable=False)
+class МоделиАвто(db.Model):
+    __tablename__ = 'МоделиАвто'
+    Код_модели = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Марка = db.Column(db.String(40), nullable=False)
+    Модель = db.Column(db.String(40), nullable=False)
+    Кузов = db.Column(db.String(40), nullable=False)
+
+class Автомобили(db.Model):
+    __tablename__ = 'Автомобили'
+    Код_автомобиля = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Код_модели = db.Column(db.Integer, db.ForeignKey('МоделиАвто.Код_модели'), nullable=False)
+    Код_владельца = db.Column(db.Integer, db.ForeignKey('Владельцы.Код_владельца'), nullable=False)
+    Регистрационный_знак = db.Column(db.String(40), nullable=False)
+    Год_выпуска = db.Column(db.Integer, nullable=False)
+    Примечание = db.Column(db.String(100))
 
 class ВидРабот(db.Model):
-    __tablename__ = 'Вид_работ'
-    Код_работы = db.Column(db.Integer, primary_key=True)
-    Наименование = db.Column(db.String(40), nullable=False)
-    Стоймость_работ = db.Column(db.Float, nullable=False)
-    Срок_выполнения = db.Column(db.Integer, nullable=False)
-    Гарантия = db.Column(db.Integer, nullable=False)
+    __tablename__ = 'ВидРабот'
+    Код_работы = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Название_работы = db.Column(db.String(100), nullable=False)
+    Стоимость_мастера = db.Column(db.Integer, nullable=False)
+
+class НомерЗапчасти(db.Model):
+    __tablename__ = 'НомерЗапчасти'
+    Номер_запчасти = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Название_запчасти = db.Column(db.String(100), nullable=False)
+    Цена = db.Column(db.Integer, nullable=False)
 
 class Заказы(db.Model):
     __tablename__ = 'Заказы'
-    Номер_заказа = db.Column(db.Integer, primary_key=True)
+    Код_заказа = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Код_автомобиля = db.Column(db.Integer, db.ForeignKey('Автомобили.Код_автомобиля'), nullable=False)
     Дата_поступления = db.Column(db.Date, nullable=False)
-    Код_работы = db.Column(db.Integer, db.ForeignKey('Вид_работ.Код_работы'), nullable=False)
-    Номер_запчасти = db.Column(db.Integer, db.ForeignKey('Номер_запчасти.Номер_запчасти'), nullable=False)
-    Количество_запчастей = db.Column(db.Integer, nullable=False)
-    Примечание = db.Column(db.String(40), nullable=False)
+    Код_работы = db.Column(db.Integer, db.ForeignKey('ВидРабот.Код_работы'), nullable=False)
+    Номер_запчасти = db.Column(db.Integer, db.ForeignKey('НомерЗапчасти.Номер_запчасти'), nullable=True)
+    Количество_запчастей = db.Column(db.Integer)
+    Примечание = db.Column(db.String(100))
+    Статус = db.Column(db.String(50), default="Ожидает выполнения")  # Выполнен / Закрыт
+    Закрыт = db.Column(db.Boolean, default=False)

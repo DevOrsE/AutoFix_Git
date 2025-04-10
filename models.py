ec9f1f3563ce8619b3e57b645d1b55daddb64d2e
DevOrsE <zhenechka.antipov.98@mail.ru>
2025-04-10 15:08:49 +0300

    Обновлён функционал регистрации, исправлено имя поля 'Терефон' → 'Телефон'

--- models.py
+++ models.py
@@ -5,8 +5,11 @@ db = SQLAlchemy()
 class Владельцы(db.Model):
     __tablename__ = 'Владельцы'
     Код_владельца = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    ФИО = db.Column(db.String(100), nullable=False)
+    Имя = db.Column(db.String(100), nullable=False)
+    Фамилия = db.Column(db.String(100), nullable=False)
+    Отчество = db.Column(db.String(100), nullable=False)
     Телефон = db.Column(db.String(20), nullable=False)
+    Примечание = db.Column(db.String(256), unique=True, nullable=False)
     Логин = db.Column(db.String(50), unique=True, nullable=False)
     Пароль = db.Column(db.String(256), nullable=False)
     Роль = db.Column(db.String(20), nullable=False, default="user")  # user, manager, admin
@@ -30,13 +33,13 @@ class Автомобили(db.Model):
 class ВидРабот(db.Model):
     __tablename__ = 'ВидРабот'
     Код_работы = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Название_работы = db.Column(db.String(100), nullable=False)
-    Стоимость_мастера = db.Column(db.Integer, nullable=False)
+    Название = db.Column(db.String(100), nullable=False)
+    Стоимость = db.Column(db.Integer, nullable=False)
 
 class НомерЗапчасти(db.Model):
     __tablename__ = 'НомерЗапчасти'
     Номер_запчасти = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Название_запчасти = db.Column(db.String(100), nullable=False)
+    Название = db.Column(db.String(100), nullable=False)
     Цена = db.Column(db.Integer, nullable=False)
 
 class Заказы(db.Model):
@@ -49,4 +52,3 @@ class Заказы(db.Model):
     Количество_запчастей = db.Column(db.Integer)
     Примечание = db.Column(db.String(100))
     Статус = db.Column(db.String(50), default="Ожидает выполнения")  # Выполнен / Закрыт
-    Закрыт = db.Column(db.Boolean, default=False)

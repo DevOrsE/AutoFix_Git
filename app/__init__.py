18454480bbee0f97723ff14761a1f288485a312e
DevOrsE <zhenechka.antipov.98@mail.ru>
2025-06-11 15:56:18 +0300

    Автоматическое создание таблиц

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,9 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
 
+    with app.app_context():
+        db.create_all()
+
     @login_manager.user_loader
     def load_user(user_id):
         return Owner.query.get(int(user_id))

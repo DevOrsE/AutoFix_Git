78fac5036c19bbaeebf4aa525edd808600d185ce
DevOrsE <zhenechka.antipov.98@mail.ru>
2025-06-15 17:04:06 +0300

    оформление заказа и создание машины в аккаунте

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -4,6 +4,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired, Length
 from app.models.models import CarModel
+from wtforms.validators import Regexp
+from wtforms import SelectField
+from datetime import datetime
+
+year_choices = [(str(y), str(y)) for y in range(2001, datetime.now().year + 1)]
 
 class RegistrationForm(FlaskForm):
     login = StringField("Login", validators=[DataRequired(), Length(min=3, max=50)])
@@ -46,8 +51,11 @@ class OrderForm(FlaskForm):
 
 class CarForm(FlaskForm):
     model_id = SelectField('Модель автомобиля', coerce=int, validators=[DataRequired()])
-    plate = StringField('Госномер', validators=[DataRequired(), Length(max=20)])
-    year = IntegerField('Год выпуска', validators=[DataRequired()])
+    plate = StringField("Госномер", validators=[
+        DataRequired(),
+        Regexp(r'^[А-Яа-я]{2}\d{3}[А-Яа-я]$', message="Формат: две буквы, три цифры, одна буква. Пример: НЕ467А")
+    ])
+    year = SelectField("Год выпуска", choices=year_choices)
     note = TextAreaField('Заметка')
     submit = SubmitField('Добавить')
 
